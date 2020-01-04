beaa780a760444d49da3cdbc28b5cc7caf356aea
ArnRso <adrien.russo@gmail.com>
2020-01-04 13:24:53 +0100

    style registration page

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
@@ -39,6 +40,10 @@ class User implements UserInterface
     /**
      * @var string The hashed password
      * @ORM\Column(type="string")
+     * @Assert\Length(
+     *     min="8",
+     *     minMessage="Minimum {length} caractères"
+     * )
      */
     private $password;
 
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -23,18 +23,22 @@ class RegistrationFormType extends AbstractType
             ->add('phone')
             ->add('email')
             ->add('password', RepeatedType::class, [
-                'type'=>PasswordType::class,
-                'mapped'=>false,
-                'first_options'=>[
-                    'label' => "Mot de passe"
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'first_options' => [
+                    'label' => false,
+                    'attr' => [
+                        'placeholder' => 'Mot de passe'
+                    ]
                 ],
-                'second_options'=>[
-                    'label'=>'Répétez votre mot de passe'
+                'second_options' => [
+                    'label' => false,
+                    'attr' => [
+                        'placeholder' => 'Répétez votre mot de passe'
+                    ]
                 ],
-                'required'=>true,
-            ])
-
-        ;
+                'required' => true,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
