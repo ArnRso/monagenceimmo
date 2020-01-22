425e05efcca6bb429f2a0c553d552c9c10cbe90c
Adrien RUSSO <adrien.russo@gmail.com>
2020-01-22 08:43:27 +0100

    minor updates

--- src/Form/BienType.php
+++ src/Form/BienType.php
@@ -17,30 +17,80 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class BienType extends AbstractType
 {
-public function buildForm(FormBuilderInterface $builder, array $options)
-{
-    $builder
-        ->add('titre', TextType::class)
-        ->add('description', TextareaType::class, [])
-        ->add('numero_rue', IntegerType::class)
-        ->add('rue', TextType::class)
-        ->add('rue_complement', TextType::class, [
-            'required' => false
-        ])
-        ->add('code_postal', IntegerType::class)
-        ->add('ville', TextType::class)
-        ->add('surface', IntegerType::class)
-        ->add('nombre_de_pieces', IntegerType::class)
-        ->add('nombre_chambres', IntegerType::class)
-        ->add('surface_terrain', IntegerType::class)
-        ->add('prix', IntegerType::class)
-        ->add('images', FileType::class, [
-            "mapped" => false, // permet d'indiquer à symfony que cet input ne correspond à aucun champ de notre entité
-            "multiple" => true, // permet d'uploader plusieurs fichiers à la fois
-            "label" => "Uploadez vos photos",
-            "required" => false
-        ]);
-}
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre', TextType::class)
+            ->add('description', TextareaType::class, [
+            ])
+            ->add('numero_rue', IntegerType::class)
+            ->add('rue', TextType::class)
+            ->add('rue_complement', TextType::class, [
+                'required' => false
+            ])
+            ->add('code_postal', IntegerType::class)
+            ->add('ville', TextType::class)
+            ->add('surface', IntegerType::class)
+            ->add('nombre_de_pieces', IntegerType::class)
+            ->add('nombre_chambres', IntegerType::class)
+            ->add('surface_terrain', IntegerType::class)
+            ->add('prix', IntegerType::class)
+            ->add('cheminee', CheckboxType::class, [
+                'required' => false,
+            ])
+            ->add('belle_vue', CheckboxType::class, [
+                'required' => false,
+            ])
+            ->add('balcon', CheckboxType::class, [
+                'required' => false,
+            ])
+            ->add('piscine', CheckboxType::class, [
+                'required' => false,
+            ])
+            ->add('ascenseur', CheckboxType::class, [
+                'required' => false,
+            ])
+            ->add('terrasse', CheckboxType::class, [
+                'required' => false,
+            ])
+            ->add('cave', CheckboxType::class, [
+                'required' => false,
+            ])
+            ->add('parking', CheckboxType::class, [
+                'required' => false,
+            ])
+            ->add('acces_handicape', CheckboxType::class, [
+                'required' => false,
+            ])
+            ->add('baignoire', CheckboxType::class, [
+                'required' => false,
+            ])
+            ->add('cuisine_separee', CheckboxType::class, [
+                'required' => false,
+            ])
+            ->add('meuble', CheckboxType::class, [
+                'required' => false,
+            ])
+            ->add('colocation', CheckboxType::class, [
+                'required' => false,
+            ])
+            ->add('wc_separes', CheckboxType::class, [
+                'required' => false,
+            ])
+            ->add('type_de_bien', EntityType::class, [
+                'class' => TypeDeBien::class
+            ])
+            ->add('chauffage', EntityType::class, [
+                'class' => Chauffage::class
+            ])
+            ->add('images', FileType::class, [
+                "mapped" => false, // permet d'indiquer à symfony que cet input ne correspond à aucun champ de notre entité
+                "multiple" => true, // permet d'uploader plusieurs fichiers à la fois
+                "label" => "Uploadez vos photos",
+                "required" => false
+            ]);
+    }
+
 
     public function configureOptions(OptionsResolver $resolver)
     {
@@ -49,3 +99,4 @@ public function buildForm(FormBuilderInterface $builder, array $options)
         ]);
     }
 }
+
